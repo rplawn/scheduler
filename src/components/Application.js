05bb95bc061ffcb8eab98b5d0160d07e99925ad3
roop <rplawn@icloud.com>
2022-07-24 14:49:28 -0700

    Built InterviewerList componentand completed story unit testing

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "components/Application.scss";
+import DayList from "components/DayList";
+
 
 //Days array
 const days = [
@@ -21,6 +23,7 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -30,7 +33,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={day}
+            setDay={setDay}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+    "interviewers__item": !props.selected
+  });
+
+  return (
+    <li className={interviewerClass}
+    onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      {props.selected && <h3> {props.name} </h3>}
+    </li>
+  )
+}
