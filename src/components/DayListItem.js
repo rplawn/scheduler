5e6d45d3b47b3100156f49b26f4509d25679c8f7
roop <rplawn@icloud.com>
2022-07-24 14:14:08 -0700

    build component that uses map to iterate over days props

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,7 +2,19 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
+const formatSpots = spots => {
+  if (!spots) {
+    return `no spots remaining`;
+  }
+  if (spots === 1) {
+    return `1 spot remaining`;
+  }
+  return `${spots} spots remaining`;
+}
+
 export default function DayListItem(props) {
+
+  const message = formatSpots(props.spots);
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots
@@ -12,7 +24,7 @@ export default function DayListItem(props) {
     <li className={dayClass} 
     onClick={() => props.setDay(props.name)}>
     <h2 className="text--regular">{props.name}</h2>
-    <h3 className="text--light">{props.spots} spots remaining</h3>
+    <h3 className="text--light">{message}</h3>
   </li>
   );
 }
