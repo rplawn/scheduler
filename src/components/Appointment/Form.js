de853671cd146eb506b41e2704139d5a7d4f3010
roop <rplawn@icloud.com>
2022-07-24 22:49:11 -0700

    update form components to use storybook props

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from 'react';
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  //Function to clear fields
+  const reset = () => {
+    setStudent("")
+    setInterviewer("")
+  }
+
+  function cancel() {
+    reset();
+    props.onCancel()
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
+            name={props.name}
             type="text"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
             placeholder="Enter Student Name"
           /*
             This must be a controlled component
@@ -19,13 +35,15 @@ export default function Form(props) {
           />
         </form>
         <InterviewerList
-        /* your code goes here */
+        interviewers={props.interviewers}
+        value={interviewer}
+        onChange={(event) => setInterviewer(event)}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger {/* your code goes here */}>Cancel</Button>
-          <Button confirm {/* your code goes here */}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onSubmit={event => event.preventDefault()} onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
