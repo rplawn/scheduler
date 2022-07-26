ddf204b6e1ecead5b261fe66d9609e9583d3e500
roop <rplawn@icloud.com>
2022-07-25 22:19:14 -0700

    built function to return appointment arr

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 
+
 export default function Appointment(props) {
   return (
     <article className="appointment">
