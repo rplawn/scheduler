cd01885d259ae221e4ca3376be340829526913a3
roop <rplawn@icloud.com>
2022-07-30 17:02:21 -0700

    reference to outdated variable

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Button from "components/Button"
 
-export default function Confirm(props) {
+//Alternative to writing props
+export default function Confirm({
+  message,
+  onCancel,
+  onConfirm
+}) {
   return (
     <main className="appointment__card appointment__card--confirm">
-      <h1 className="text--semi-bold">{props.message}</h1>
+      <h1 className="text--semi-bold">{message}</h1>
       <section className="appointment__actions">
-        <Button danger onClick={props.onCancel}>Cancel</Button>
-        <Button danger onClick={props.onConfirm}>Confirm</Button>
+        <Button danger onClick={onCancel}>Cancel</Button>
+        <Button danger onClick={onConfirm}>Confirm</Button>
       </section>
     </main>
   )
